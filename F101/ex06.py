aba70e4846fa6715629347842786d6833a4a766d
AnabelaV94 <anabela.alves.veiga@hotmail.com>
2025-01-25 13:14:02 +0000

    ultimas alterações

--- F101/ex06.py
+++ F101/ex06.py
@@ -1,3 +1,11 @@
 '''Faz um programa que receba três parâmetros inteiros (horas, minutos e segundos) e 
 converta o resultado para segundos, devolvendo o output para o ecrã (deverá ser 
 criado o ficheiro ex06.py).'''
+
+valor_horas = int(input("introduza as horas\n"))
+valor_minutos = int(input("introduza os minutos\n"))
+valor_segundos = int(input("introduza os segundos\n"))
+valor_final_segundos = (valor_horas * 3600) + (valor_minutos * 60) + valor_segundos
+print (f" o valor em segundos é {valor_final_segundos}")
+
+#infos: https://tortoisegit.org/
--- F101/ex07.py
+++ F101/ex07.py
@@ -2,3 +2,8 @@
 combustível consumido por um carro num percurso. Calcula o consumo km/l e escreve 
 uma mensagem de acordo com o resultado obtido. (deverá ser criado o ficheiro 
 ex07.py). '''
+
+distância = float(input("introduza a distância percorrida\n"))
+combustivel = float(input("introduza a quantidade em litros de combustível\n"))
+valor_consumo= distância/combustivel
+print (f" consumo km/l = {valor_consumo}")

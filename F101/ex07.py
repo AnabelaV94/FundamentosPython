029223642d65148dea4def8ec123ab44eb386b01
AnabelaV94 <anabela.alves.veiga@hotmail.com>
2025-01-25 13:16:29 +0000

    +

--- F101/ex07.py
+++ F101/ex07.py
@@ -7,3 +7,11 @@ distância = float(input("introduza a distância percorrida\n"))
 combustivel = float(input("introduza a quantidade em litros de combustível\n"))
 valor_consumo= distância/combustivel
 print (f" consumo km/l = {valor_consumo}")
+
+# Mensagem baseada no valor do consumo
+if valor_consumo < 8:
+    print("Consumo muito alto!")
+elif 8 <= valor_consumo <= 12:
+    print("Consumo moderado.")
+else:
+    print("Consumo eficiente!")
